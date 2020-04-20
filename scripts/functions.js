9375f2d71e814ba7f6b7d256ec93dd16a8ecea7b
Sang Min Oh <sangminoh715@gmail.com>
2020-04-19 20:07:07 -0700

    Added 'To-Top' Button

--- scripts/functions.js
+++ scripts/functions.js
@@ -43,3 +43,25 @@ function removeViewerAndContent() {
     
     document.body.classList.remove("fixedView");
 }
+
+function scrollListener() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > (document.body.clientHeight / 4)) {
+            const button = document.createElement("img");
+            button.setAttribute("class", "toTopButton");
+            button.setAttribute("src", "images/toTopButton.png");
+            button.setAttribute("onclick", "moveBackToTop()");
+            
+            document.body.appendChild(button);
+        } else {
+            const button = $(".toTopButton");
+            if(button != null) {
+                button.remove();
+            }
+        }
+    });
+}
+
+function moveBackToTop() {
+    window.scrollTo(0, 0);
+}
